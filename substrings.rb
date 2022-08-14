4b267abe76344c89075cd1f2ef5cd426094d6565
TurtleNaTree <kmcqueen108121@hotmail.com>
2022-08-14 09:53:36 +0000

    changed the substrings function to use regex. Program is finished now

--- substrings.rb
+++ substrings.rb
@@ -4,7 +4,7 @@ word = "My mind to me a kingdom is;
 Such present joys therein I find,
 That it excels all other bliss
 That earth affords or grows by kind:
-Though much I want that most would have,
+Though much Ii want that most would have,
 Yet still my mind forbids to crave …"
 dictionary = ["that", "I", "crave"]
 
@@ -15,13 +15,13 @@ def substrings (text, dictionary)
     substring_tally => {}
     text_array.reduce(0) {|count, substring|
         dictionary.each do |word| 
-            if word.downcase == substring.downcase 
-                substring_tally[substring.downcase] ? substring_tally[substring.downcase] += 1 : substring_tally[substring.downcase] = 1       
+            unless substring.scan(/#{word}/i).empty? 
+                substring_tally[word.downcase] ? substring_tally[word.downcase] += substring.scan(/#{word}/i).count : substring_tally[word.downcase] = 1       
             end
         substring_tally  
         end 
     }
-    print substring_tally
+    puts substring_tally
 end
 
 substrings(word, dictionary)
